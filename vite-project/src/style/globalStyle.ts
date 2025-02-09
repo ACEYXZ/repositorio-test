09959ec330bb79a91c97bf855e30c152f7420e16
Joao Pedro Nicacio <jpn.athademo@gmail.com>
2025-02-09 17:29:31 -0300

    feats: add feats in general

---
+++ vite-project/src/style/globalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    body, html{ 
+        margin: 0;
+        padding: 0;
+        font-family: 
+    }
+    h1, h2, h3, li, ul, p, a {
+        margin: 0;
+        padding: 0
+    }
+`
+
